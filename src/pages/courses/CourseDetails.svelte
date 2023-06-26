a8e14eb8fd1ccadfc5e222fbca5e18365f0b935e
Invited User iuapk1uo <iuapk1uo@mail.gitlabdemo.cloud>
2023-06-26 15:42:08 +0200

    add holes

--- src/pages/courses/CourseDetails.svelte
+++ src/pages/courses/CourseDetails.svelte
@@ -11,16 +11,29 @@
         // See https://svelte.dev/tutorial/reactive-statements
         id = params.id;
         getCourse();
+        getHoles();
         getPlayersForCourse();
     }
 
     let course = {};
     let players = [];
+    let holes = [];
 
     function getCourse() {
         axios.get("https://golf-course-backend-c071f3efa027.herokuapp.com/api/course/" + id)
             .then((response) => {
                 course = response.data;
+            });
+    }
+
+    function getHoles() {
+        axios.get("https://golf-course-backend-c071f3efa027.herokuapp.com/api/holes/" + id)
+            .then((response) => {
+            if (Array.isArray(response.data)) {
+                holes = response.data;
+            } else {
+                holes = [response.data];
+            }
             });
     }
 
@@ -41,6 +54,26 @@
     <h1 class="mt-3">Course (ID: {id})</h1>
     <p>Location: {course.location}</p>
     <p>Size: {course.size}</p>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Hole Number</th>
+                <th>level</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each holes as hole}
+                <tr>
+                    <td>
+                        {hole.hole_id}
+                    </td>
+                    <td>
+                        {hole.level}
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
     <table class="table">
         <thead>
             <tr>
